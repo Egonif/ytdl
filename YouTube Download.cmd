4be0e65d08d4bc94ef518b5e273edc9a29437e32
unknown <egon.leteurtre@gmail.com>
2024-03-31 14:03:40 +0200

    Add loop for one launch

--- YouTube Download.cmd
+++ YouTube Download.cmd
@@ -1,10 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set /p "url=Entrer le lien de la vidéo: "
-set /p "format=Entrer le format de la vidéo (mp3/mp4): "
+@REM Ask for destination folder
 set "browseforfolder="(new-object -COM 'Shell.Application').BrowseForFolder(0,'Please choose a folder',0,0).self.path""
 for /f "usebackq delims=" %%I in (`powershell %browseforfolder%`) do set "destinationfolder=%%I"
+
+:loop
+cls
+set /p "url=Entrer le lien de la vidéo: "
+set /p "format=Entrer le format de la vidéo (mp3/mp4): "
 
 if "%format%"=="mp3" (
     @REM Download mp3
@@ -14,3 +18,4 @@ if "%format%"=="mp4" (
     @REM Download mp4
     yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" -o "!destinationfolder!\%%(title)s.%%(ext)s" %url%
 )
+goto:loop
